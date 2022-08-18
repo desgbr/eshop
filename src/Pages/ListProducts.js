1b08c3f323a55605cb3d872d582f7749aff03294
nesrinmohamed <94196690+nesrinmohamed@users.noreply.github.com>
2022-08-18 21:53:01 +0200

    udates

--- src/Pages/ListProducts.js
+++ src/Pages/ListProducts.js
@@ -1,22 +1,29 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-
-function ListProducts() {
+import{GiShoppingCart} from 'react-icons/gi'
+function ListProducts({product }) {
 return(
- <div >
- <Card style={{ width: '18rem' }}>
-    <Card.Img variant="top" src= ''/>
-    <Card.Body>
-      <Card.Title></Card.Title>
-      <Card.Text>
-        Some quick example text to build on the card title and make up the
-        bulk of the card's content.
-      </Card.Text>
-      <Button variant="primary">Go somewhere</Button>
+  <>
+  <div className='card-item'>
+  <Card style={{ width: '15rem', border:"none" }}>
+  <div className="top-card d-flex  justify-content-between m-2">
+    <span className='bg-success text-white p-1 rounded h-25'>{product.rating}</span>
+    <p className='bg-danger text-white p-1 rounded'>{product.category}</p>
+  </div>
+   <Card.Img variant="top" src= {product.thumbnail}/> <Card.Body>
+      <Card.Title>{product.title}</Card.Title>
+      <Card.Text> {product.description}  </Card.Text>
     </Card.Body>
-  </Card>
+    <div className="btn btn-card d-flex align-items-center justify-content-between">
+<p className='price' style={{fontSize:"20px"}}>{product.price}LE</p>
+<Button variant="warning"> <GiShoppingCart/>add to card</Button>
 </div>
+  </Card>
+
+</div>
+  </>
+ 
   )
 
 }
